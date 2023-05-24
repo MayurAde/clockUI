f4d4d88a7f6d0e8f7a18d999e66dbf789387b325
Mayur Ade <mayurade244@gmail.com>
2023-05-24 14:21:25 +0530

    synced analog and digital clock

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-// function rotateHand() {
-//   const hand = document.querySelector(".hand");
-//   const now = new Date();
-//   const seconds = now.getSeconds();
-//   const degrees = (seconds / 60) * 360;
-//   hand.style.transform = `rotate(${degrees}deg)`;
-// }
+const dispNum = (num) => {
+  if (Number(num) < 10) {
+    return "0" + String(num);
+  } else {
+    return String(num);
+  }
+};
 
-// setInterval(rotateHand, 1000);
-
-setInterval(() => {
-  const currentDate = new Date();
-  const seconds = currentDate.getSeconds();
-  const degree = (seconds / 60) * 360;
-  document.querySelector(".hand").style.transform = `rotate(${degree}deg)`;
-}, 100);
-
+document.querySelector(".hand").style.transform = "rotate(180deg)";
 const changeTime = () => {
   const hour = document.querySelector(".hour");
   const minutes = document.querySelector(".minutes");
   const seconds = document.querySelector(".seconds");
 
   const currentDate = new Date();
-  hour.textContent = currentDate.getHours();
-  minutes.textContent = currentDate.getMinutes();
-  seconds.textContent = currentDate.getSeconds();
+  const degree = (currentDate.getSeconds() / 60) * 360;
+
+  document.querySelector(".hand").style.transform = `rotate(${degree - 90}deg)`;
+
+  hour.textContent = dispNum(currentDate.getHours());
+  minutes.textContent = dispNum(currentDate.getMinutes());
+  seconds.textContent = dispNum(currentDate.getSeconds());
 };
 
-setInterval(changeTime, 100);
+setInterval(changeTime, 1000);
